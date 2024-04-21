9b802f6fec2237c4226a9dbf6cd5517568f1e5c3
Dominik Duda <dominikduda191@gmail.com>
2024-04-21 11:17:39 +0200

    script.js fix

--- lab3/script.js
+++ lab3/script.js
@@ -1,4 +1,4 @@
-// alert("Hello World")
+alert("Hello World");
 
 a = 10;
 b = 20;
@@ -9,43 +9,43 @@ odejmowanie = a - b - c;
 mnożenie = a * b * c;
 dzielenie = a / b / c;
 
-// alert("Dodawanie : " + dodawanie)
-// alert("Odejmowanie : " + odejmowanie)
-// alert("Mnożenie : " + mnożenie)
-// alert("Dzielenie : " + dzielenie)
+alert("Dodawanie : " + dodawanie);
+alert("Odejmowanie : " + odejmowanie);
+alert("Mnożenie : " + mnożenie);
+alert("Dzielenie : " + dzielenie);
 
-// console.log("Dodawanie : " + dodawanie);
-// console.log("Odejmowanie : " + odejmowanie);
-// console.log("Mnożenie : " + mnożenie);
-// console.log("Dzielenie : " + dzielenie);
+console.log("Dodawanie : " + dodawanie);
+console.log("Odejmowanie : " + odejmowanie);
+console.log("Mnożenie : " + mnożenie);
+console.log("Dzielenie : " + dzielenie);
 
 document.addEventListener("DOMContentLoaded", function () {
-  //   document.getElementById("add").innerHTML = "Dodawnie" + dodawanie;
-  //   document.getElementById("sub").innerHTML = "Odejmownie" + odejmowanie;
-  //   document.getElementById("mul").innerHTML = "Mnożenie" + mnożenie;
-  //   document.getElementById("divi").innerHTML = "Dzielenie" + dzielenie;
-  //   for (let i = 0; i <= 100; i += 2) {
-  //     console.log(i);
-  //   }
-  //   const imie = prompt("Jak Ci na imię? ");
-  //   console.log("Witaj : " + imie);
-  //   alert("Cześć " + imie);
-  //   a = parseFloat(prompt("Podaj dlugość a"));
-  //   b = parseFloat(prompt("Podaj dlugość b"));
-  //   c = parseFloat(prompt("Podaj dlugość c"));
-  //   if (a + b > c && a + c > b && b + c > a) {
-  //     const pole2 = (a + b + c) / 2;
-  //     console.log(pole2);
-  //     alert("Pole trójkąta : " + pole2);
-  //     const pole = Math.sqrt(pole2 * (pole2 - a) * (pole2 - b) * (pole2 - c));
-  //     console.log("Pole trójkąta : " + pole);
-  //   } else {
-  //     console.log("Nie można skonstruować trójkąta!!!");
-  //   }
-  //   const first = parseFloat(prompt("Podaj pierwszą liczbę : "));
-  //   const second = parseFloat(prompt("Podaj drugą liczbę : "));
-  //   const wynik = first + second;
-  //   alert("Wynik dodawania tych 2 liczb to : " + wynik);
+  document.getElementById("add").innerHTML = "Dodawnie" + dodawanie;
+  document.getElementById("sub").innerHTML = "Odejmownie" + odejmowanie;
+  document.getElementById("mul").innerHTML = "Mnożenie" + mnożenie;
+  document.getElementById("divi").innerHTML = "Dzielenie" + dzielenie;
+  for (let i = 0; i <= 100; i += 2) {
+    console.log(i);
+  }
+  const imie = prompt("Jak Ci na imię? ");
+  console.log("Witaj : " + imie);
+  alert("Cześć " + imie);
+  a = parseFloat(prompt("Podaj dlugość a"));
+  b = parseFloat(prompt("Podaj dlugość b"));
+  c = parseFloat(prompt("Podaj dlugość c"));
+  if (a + b > c && a + c > b && b + c > a) {
+    const pole2 = (a + b + c) / 2;
+    console.log(pole2);
+    alert("Pole trójkąta : " + pole2);
+    const pole = Math.sqrt(pole2 * (pole2 - a) * (pole2 - b) * (pole2 - c));
+    console.log("Pole trójkąta : " + pole);
+  } else {
+    console.log("Nie można skonstruować trójkąta!!!");
+  }
+  const first = parseFloat(prompt("Podaj pierwszą liczbę : "));
+  const second = parseFloat(prompt("Podaj drugą liczbę : "));
+  const wynik = first + second;
+  alert("Wynik dodawania tych 2 liczb to : " + wynik);
 
   const numberOne = parseFloat(prompt("Podaj pierwszą liczbę : "));
   const numberTwo = parseFloat(prompt("Podaj drugą liczbę : "));
